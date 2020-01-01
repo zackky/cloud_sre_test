0fa8ab581a82612e38bc2cd3484ddf4f0e965f7a
Zackky Muhammad <mzackky@gmail.com>
2020-01-02 01:00:43 +0700

    Fix deprecation warning on some modules

--- solutions/terraform/modules/cdn/main.tf
+++ solutions/terraform/modules/cdn/main.tf
@@ -85,7 +85,7 @@ resource "aws_cloudfront_distribution" "cdn" {
   }
 
   viewer_certificate {
-    acm_certificate_arn = "${aws_acm_certificate.cert.arn}"
+    acm_certificate_arn = aws_acm_certificate.cert.arn
     ssl_support_method  = "sni-only"
   }
 
--- solutions/terraform/modules/dns/main.tf
+++ solutions/terraform/modules/dns/main.tf
@@ -16,7 +16,7 @@ resource "aws_route53_zone" "dns" {
 resource "aws_route53_record" "records" {
   count = length(var.dns_records)
 
-  zone_id = "${aws_route53_zone.dns.zone_id}"
+  zone_id = aws_route53_zone.dns.zone_id
   name    = var.dns_records[count.index].name
   type    = var.dns_records[count.index].type
   ttl     = var.ttl
